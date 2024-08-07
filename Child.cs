ddc625d68fa48d0bc9e741facf93e0d6408a5bdc
vladk <vladk@DESKTOP-8VBNNO7>
2024-08-07 19:15:09 +0300

    Добавьте файлы проекта.

---
+++ Child.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace ConsoleApp10
+{
+    internal class Child
+    {
+        private string name;
+        private string last_name;
+        private int age;
+        
+        public Child(string Name, string Last_name, int Age)
+        {
+            name = Name;
+            age = Age;
+            last_name = Last_name;
+            Console.WriteLine($"Name & last name:{name} {last_name} \nAge:{age}");
+        }
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp10;
+
+class HelloWorld
+{ 
+    static void Main()
+    {
+        Child child = new Child("John","Doe", 5);
+    }
+}
+
