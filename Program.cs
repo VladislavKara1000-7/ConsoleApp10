927c921c04c1841fdd919837634f5e67997f5b64
VladislavKara1000-7 <vk6844388@gmail.com>
2024-10-11 23:15:09 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ class HelloWorld
 { 
     static void Main()
     {
-        Child child = new Child("John","Doe", 5);
+        Child child1 = new Child("John","Doe", 5);
+        Child child2 = new Child("Jane","Doe",5);
     }
 }
 
